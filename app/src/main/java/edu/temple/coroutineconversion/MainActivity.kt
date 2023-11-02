6c0c963cbed8bce851936c21845769b01535dae7
finnbassill <tun39698@temple.edu>
2023-11-02 11:48:01 -0400

    Implemented coroutine in place of thread in MainActivity

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,13 +28,15 @@ class MainActivity : AppCompatActivity() {
 
         cakeImageView = findViewById(R.id.imageView)
 
-        findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
+        val scope = CoroutineScope(Job() + Dispatchers.Default)
+
+        findViewById<Button>(R.id.revealButton).setOnClickListener() {
+            scope.launch {
                 repeat(100) {
                     handler.sendEmptyMessage(it)
                     Thread.sleep(40)
                 }
-            }.start()
+            }
         }
     }
 }
